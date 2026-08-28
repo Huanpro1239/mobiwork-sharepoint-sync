aa301ed2d565edce80fa0f30659feb6f8f9f8899
Nguyễn Sĩ Huấn <nguyensihuan711994@gmail.com>
2026-08-28 10:45:37 +0700

    Make planning structure regression test unittest-compatible

--- tests/test_planning_domain_structure.py
+++ tests/test_planning_domain_structure.py
@@ -1,3 +1,5 @@
+import unittest
+
 from src.planning import formula_port
 from src.planning.domain.demand import forecast_by_month
 from src.planning.domain.materials import build_material_inbound_plan
@@ -5,8 +7,19 @@ from src.planning.domain.production import build_weekly_production_plan
 from src.planning.domain.purchasing import build_purchase_plan
 
 
-def test_formula_port_is_compatibility_facade():
-    assert formula_port.forecast_by_month is forecast_by_month
-    assert formula_port.build_material_inbound_plan is build_material_inbound_plan
-    assert formula_port.build_purchase_plan is build_purchase_plan
-    assert formula_port.build_weekly_production_plan is build_weekly_production_plan
+class PlanningDomainStructureTests(unittest.TestCase):
+    def test_formula_port_is_compatibility_facade(self):
+        self.assertIs(formula_port.forecast_by_month, forecast_by_month)
+        self.assertIs(
+            formula_port.build_material_inbound_plan,
+            build_material_inbound_plan,
+        )
+        self.assertIs(formula_port.build_purchase_plan, build_purchase_plan)
+        self.assertIs(
+            formula_port.build_weekly_production_plan,
+            build_weekly_production_plan,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
